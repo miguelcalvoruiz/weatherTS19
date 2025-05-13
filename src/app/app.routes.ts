bd0950ff50313fc07420fa1164d016518973996a
Miguel <miguelcalvoruiz6@gmail.com>
2025-05-13 20:38:41 +0200

    feat: añadidos mayoria de los estilos egenrales de mi plantilla de diseño y creado el header con la funcionalidad de la busqueda

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'current-location', component: AppComponent },
+    { path: '', redirectTo: '/current-location', pathMatch: 'full' },
+    { path: '**', component: AppComponent }
+];
